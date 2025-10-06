a144f010a4dde9afddee874e579d3549747f6d9b
omidsdgi <omid69sdgi@gmail.com>
2025-10-06 19:49:47 +0330

    feat: implement server data fetching with async Page and client Counter component

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,8 +1,21 @@
-function Page() {
+import Counter from "@/app/components/Counter";
+
+async function Page() {
+    const res=await fetch("https://jsonplaceholder.typicode.com/users");
+    const data=await res.json();
+    console.log(data);
     return (
-        <h1>
-        Mehraneh cabins
-        </h1>
+        <>
+            <h1>
+                Mehraneh cabins
+            </h1>
+            <ul>
+                {data.map((user) =>(
+                    <li key={user.id}>{user.name}</li>
+                ))}
+            </ul>
+            <Counter user={data}/>
+        </>
     );
 }
 
