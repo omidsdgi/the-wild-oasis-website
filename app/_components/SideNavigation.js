024f681434c2c4d2e50fb8d5850220d614a34e2c
omidsdgi <omid69sdgi@gmail.com>
2025-10-13 09:54:55 +0330

    feat(app/side-navigation): highlight active navigation link

--- app/_components/SideNavigation.js
+++ app/_components/SideNavigation.js
@@ -1,11 +1,15 @@
+"use client"
 import {
   CalendarDaysIcon,
   HomeIcon,
   UserIcon,
 } from '@heroicons/react/24/solid';
 import SignOutButton from './SignOutButton';
+import {usePathname} from "next/navigation";
+import Link from "next/link";
 
 const navLinks = [
+
   {
     name: 'Home',
     href: '/account',
@@ -24,18 +28,19 @@ const navLinks = [
 ];
 
 function SideNavigation() {
+  const pathname =usePathname()
   return (
     <nav className='border-r border-primary-900'>
       <ul className='flex flex-col gap-2 h-full text-lg'>
         {navLinks.map((link) => (
           <li key={link.name}>
-            <a
-              className={`py-3 px-5 hover:bg-primary-900 hover:text-primary-100 transition-colors flex items-center gap-4 font-semibold text-primary-200`}
+            <Link
+              className={`py-3 px-5 hover:bg-primary-900 hover:text-primary-100 transition-colors flex items-center gap-4 font-semibold text-primary-200 ${pathname === link.href ?"bg-primary-950":""}`}
               href={link.href}
             >
               {link.icon}
               <span>{link.name}</span>
-            </a>
+            </Link>
           </li>
         ))}
 
