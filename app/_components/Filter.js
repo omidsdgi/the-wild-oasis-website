cf4f60fc21978b74ca023176879140fc049034db
omidsdgi <omid69sdgi@gmail.com>
2025-10-13 23:09:01 +0330

    feat: assign key to Suspense fallback and create reusable Button component

--- app/_components/Filter.js
+++ app/_components/Filter.js
@@ -37,7 +37,6 @@ function Filter() {
             >
                 4&mdash;7 guests
             </Button>
-
             <Button
                 filter="large"
                 activeFilter={activeFilter}
@@ -51,7 +50,8 @@ function Filter() {
 export default Filter
 function Button({filter,activeFilter,handleFilter,children}) {
     return(
-        <button  className={`px-5 py-2 hover:bg-primary-700 ${filter===activeFilter ? "bg-primary-700 text-primary-50": ""}`}
+        <button  className={`px-5 py-2 hover:bg-primary-700
+                    ${filter===activeFilter ? "bg-primary-700 text-primary-50": ""}`}
                  onClick={() => handleFilter(filter)}>
             {children}
         </button>
