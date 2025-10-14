2feb55ae252d77c5fcbcff8b90b3ceae1ac58e72
omidsdgi <omid69sdgi@gmail.com>
2025-10-14 14:08:08 +0330

    feat: implement data fetching strategies for reservation section

---
+++ app/_components/Reservation.js
@@ -0,0 +1,18 @@
+import DateSelector from "@/app/_components/DateSelector";
+import ReservationForm from "@/app/_components/ReservationForm";
+import {getBookedDatesByCabinId, getCabin, getSettings} from "@/app/_lib/data-service";
+
+async function Reservation({cabin}) {
+    const [settings,bookedDates]=await Promise.all([
+        getSettings(),
+        getBookedDatesByCabinId(cabin.id),
+    ])
+    return (
+        <div className="grid grid-cols-[4fr_3fr] gap-1  border-primary-800 max-h-[400px]">
+            <DateSelector settings={settings} bookedDates={bookedDates} cabin={cabin}/>
+            <ReservationForm cabin={cabin}/>
+        </div>
+    );
+}
+
+export default Reservation
--- app/cabins/[cabinId]/page.js
+++ app/cabins/[cabinId]/page.js
@@ -1,7 +1,9 @@
 import Cabin from "@/app/_components/Cabin";
-import {getBookedDatesByCabinId, getCabin, getCabins, getSettings} from "@/app/_lib/data-service";
-import ReservationForm from "@/app/_components/ReservationForm";
-import DateSelector from "@/app/_components/DateSelector";
+import {getCabin, getCabins} from "@/app/_lib/data-service";
+
+import Reservation from "@/app/_components/Reservation";
+import {Suspense} from "react";
+import Spinner from "@/app/_components/Spinner";
 
 export async function generateMetadata({ params }) {
     const { name } = await getCabin(params.cabinId);
@@ -12,15 +14,12 @@ export async function generateStaticParams() {
     const cabins = await getCabins();
 
     const ids = cabins.map((cabin) => ({ cabinId: String(cabin.id) }));
-
     return ids;
 }
 
 export default async function Page({ params }) {
     const cabin = await getCabin(params.cabinId);
-    const setting=await getSettings()
-    const bookedDates=await getBookedDatesByCabinId(params.cabinId);
-
+    const {id,name,maxCapacity,regularPrice,discount,image,description}=cabin
     return (
         <div className="max-w-6xl mx-auto mt-8">
             <Cabin cabin={cabin} />
@@ -29,10 +28,9 @@ export default async function Page({ params }) {
                 <h2 className="text-5xl font-semibold text-center mb-10 text-accent-400">
                     Reserve {cabin.name} today. Pay on arrival.
                 </h2>
-                <div className="grid grid-cols-[4fr_3fr] gap-1  border-primary-800 max-h-[400px]">
-                    <DateSelector/>
-                    <ReservationForm/>
-                </div>
+                <Suspense fallback={<Spinner />}>
+                <Reservation cabin={cabin} />
+                </Suspense>
             </div>
         </div>
     );
