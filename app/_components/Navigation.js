0e683afc90ff127fa6f468926fc102aa5f77d86f
omidsdgi <omid69sdgi@gmail.com>
2025-10-15 21:17:22 +0330

    feat(auth): personalize UI with logged-in user avatar display

--- app/_components/Navigation.js
+++ app/_components/Navigation.js
@@ -1,28 +1,46 @@
 import Link from "next/link";
+import {auth} from "@/app/_lib/auth";
+import Image from "next/image";
 
-export default function Navigation() {
-  return (
-    <nav className="z-10 text-xl">
-      <ul className="flex gap-16 items-center">
-        <li>
-          <Link href="/cabins" className="hover:text-accent-400 transition-colors">
-            Cabins
-          </Link>
-        </li>
-        <li>
-          <Link href="/about" className="hover:text-accent-400 transition-colors">
-            About
-          </Link>
-        </li>
-        <li>
-          <Link
-            href="/account"
-            className="hover:text-accent-400 transition-colors"
-          >
-            Guest area
-          </Link>
-        </li>
-      </ul>
-    </nav>
-  );
+export default async function Navigation() {
+    const session=await auth()
+    console.log(session)
+    return (
+        <nav className="z-10 text-xl">
+            <ul className="flex gap-16 items-center">
+                <li>
+                    <Link href="/cabins" className="hover:text-accent-400 transition-colors">
+                        Cabins
+                    </Link>
+                </li>
+                <li>
+
+                    <Link href="/about" className="hover:text-accent-400 transition-colors">
+                        About
+                    </Link>
+                </li>
+                <li>
+                    {session?.user?.image ?(
+                        <Link
+                            href="/account"
+                            className="hover:text-accent-400 transition-colors flex items-center justify-center gap-3 relative"
+                        >
+                                <Image className="rounded-full object-cover"
+                                       src={session.user.image} height={32} width={32}
+                                       alt={session.user.name} referrerPolicy="no-referrer" />
+                                <span> Guest area</span>
+                        </Link>
+                        ):(
+                        <Link
+                        href="/account"
+                        className="hover:text-accent-400 transition-colors"
+                        >
+                        Guest area
+                        </Link>
+                        )}
+
+                </li>
+            </ul>
+        </nav>
+    );
 }
