f1e4ac6148f9e7d9124787844af684395a061af3
omidsdgi <omid69sdgi@gmail.com>
2025-10-15 21:37:30 +0330

    feat(auth): conditionally render reservation form or login message based on user session

--- app/_components/Reservation.js
+++ app/_components/Reservation.js
@@ -1,16 +1,19 @@
 import DateSelector from "@/app/_components/DateSelector";
 import ReservationForm from "@/app/_components/ReservationForm";
 import {getBookedDatesByCabinId, getCabin, getSettings} from "@/app/_lib/data-service";
+import {auth} from "@/app/_lib/auth";
+import LoginMessage from "@/app/_components/LoginMessage";
 
 async function Reservation({cabin}) {
+    const session=await auth()
     const [settings,bookedDates]=await Promise.all([
         getSettings(),
         getBookedDatesByCabinId(cabin.id),
     ])
     return (
         <div className="grid grid-cols-[4fr_3fr] gap-1 border border-primary-800 max-h-[400px]">
-            <DateSelector settings={settings} bookedDates={bookedDates} cabin={cabin}/>
-            <ReservationForm cabin={cabin}/>
+            <DateSelector settings={settings} bookedDates={bookedDates} cabin={cabin} />
+            {session.user ? <ReservationForm cabin={cabin} user={session.user}/> : <LoginMessage/>}
         </div>
     );
 }
