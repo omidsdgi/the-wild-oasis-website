d78c06b401223d231df83b566b5fd598febf266a
omidsdgi <omid69sdgi@gmail.com>
2025-10-17 18:21:32 +0330

    feat(delete-reservation): implement delete reservation

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -1,10 +1,9 @@
 "use server"
 
-
-
 import {auth, signIn, signOut} from "./auth";
 import {supabase} from "@/app/_lib/supabase";
 import {revalidatePath} from "next/cache";
+import {getBookings} from "@/app/_lib/data-service";
 
 export async function updateGuest(formData) {
     const session = await auth();
@@ -14,12 +13,24 @@ export async function updateGuest(formData) {
     if (!/^[a-zA-Z0-9]{6,12}$/.test(nationalID))throw new Error("Please provide a valid national ID");
     const updateData = {nationality,nationalID,countryFlag};
 
-    const { data, error } = await supabase
+    const { error } = await supabase
         .from('guests')
         .update(updateData)
         .eq('id', session.user.guestId)
     if (error) throw new Error('Guest could not be updated');
     revalidatePath("/account/profile");
+}
+export async function deleteReservation(bookingId) {
+    const session = await auth();
+    if (!session) throw new Error("You must be logged in");
+
+    const guestBookings=getBookings(session.user.guestId);
+    const guestBookingIds=guestBookings.map(booking=>booking.id);
+    if (!guestBookingIds.includes(bookingId)) throw new Error("You are not allowed to delete this booking");
+
+    const { error } = await supabase.from('bookings').delete().eq('id', bookingId);
+    if (error) throw new Error('Booking could not be deleted');
+    revalidatePath("/account/reservations");
 }
 
 
@@ -27,6 +38,8 @@ export async function signInAction() {
     await signIn
     await signIn("google",{redirectTo:"/account"})
 }
+
+
 export async function signOutAction() {
     await signOut({redirectTo:"/"})
 }
