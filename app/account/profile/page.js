18f1a03572740b4053a658231f07d86c1bdedf10
omidsdgi <omid69sdgi@gmail.com>
2025-10-17 10:25:05 +0330

    feat: implement profile update using a server action

--- app/account/profile/page.js
+++ app/account/profile/page.js
@@ -1,30 +1,36 @@
 import UpdateProfileForm from "@/app/_components/UpdateProfileForm";
 import SelectCountry from "@/app/_components/SelectCountry";
+import {auth} from "@/app/_lib/auth";
+import {getGuest} from "@/app/_lib/data-service";
 
 export const metadata={
-  title: "Update Profile",
+    title: "Update Profile",
 }
 
-export default function Page() {
-    const nationality = "portugal";
-  return (
-    <div>
-      <h2 className="font-semibold text-2xl text-accent-400 mb-4">
-        Update your guest profile
-      </h2>
+export default async function Page() {
+    const session = await auth();
+    const guest = await getGuest(session.user.email);
+    const {nationality}=guest
 
-      <p className="text-lg mb-8 text-primary-200">
-        Providing the following information will make your check-in process
-        faster and smoother. See you soon!
-      </p>
-<UpdateProfileForm>
-    <SelectCountry
-        name="nationality"
-        id="nationality"
-        className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm"
-        defaultCountry={nationality}
-    />
-</UpdateProfileForm>
-    </div>
-  );
+    return (
+        <div>
+            <h2 className="font-semibold text-2xl text-accent-400 mb-4">
+                Update your guest profile
+            </h2>
+
+            <p className="text-lg mb-8 text-primary-200">
+                Providing the following information will make your check-in process
+                faster and smoother. See you soon!
+            </p>
+
+            <UpdateProfileForm guest={guest}>
+                <SelectCountry
+                    name="nationality"
+                    id="nationality"
+                    className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm"
+                />
+            </UpdateProfileForm>
+        </div>
+    );
 }
+
