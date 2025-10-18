6cbede5ba231b9c6bc96aa242bc946916a8c5bc9
omidsdgi <omid69sdgi@gmail.com>
2025-10-18 20:51:39 +0330

    Replaced the regular submit button with SubmitButton component

--- app/_components/ReservationForm.js
+++ app/_components/ReservationForm.js
@@ -3,6 +3,7 @@ import {useReservation} from "@/app/_components/ReservationContext";
 import Image from "next/image";
 import {differenceInDays} from "date-fns";
 import {createBooking} from "@/app/_lib/actions";
+import SubmitButton from "@/app/_components/submitButton";
 
 function ReservationForm({cabin,user}) {
     const {range,resetRange}=useReservation();
@@ -79,9 +80,7 @@ function ReservationForm({cabin,user}) {
                 <div className='flex justify-end items-center gap-6'>
                     <p className='text-primary-300 text-base'>Start by selecting dates</p>
 
-                    <button className='bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300'>
-                        Reserve now
-                    </button>
+                    <SubmitButton pendingLabel="Reserving ...">Reserve now</SubmitButton>
                 </div>
             </form>
         </div>
